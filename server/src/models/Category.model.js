54402f6648a992d2f2cf851dec4637993682ee2a
Carlos Diaz <58927717+Carlos13-Lab@users.noreply.github.com>
2022-11-15 09:56:08 -0400

    Update Category.model.js

--- server/src/models/Category.model.js
+++ server/src/models/Category.model.js
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const categoriesSchema = new Schema({
     name: {
         type: String,
-        enum: ['Frontend', 'Backend', 'QA', 'Testing', 'UX/UI', 'DevOps', 'Architecture', 'Data Science','Machine learning'],
+        enum: ['Frontend', 'Backend', 'QA', 'Testing', 'UX/UI', 'DevOps', 'Architecture', 'Data Science','Machine Learning'],
         default: 'Frontend',
         require: true,
     },
