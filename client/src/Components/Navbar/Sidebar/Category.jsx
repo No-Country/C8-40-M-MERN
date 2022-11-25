8e2bf2bbd0c9d9b0300b1ae7cb820e22b8420b60
Martin Cristina Darroux <88113385+martincrisdarr@users.noreply.github.com>
2022-11-25 10:22:42 -0300

    fixing-navbar-git

--- client/src/Components/Navbar/Sidebar/Category.jsx
+++ client/src/Components/Navbar/Sidebar/Category.jsx
@@ -10,7 +10,7 @@ function Category({ elem, title }) {
       'flex gap-2 items-center mt-2 pl-[19px] py-2 w-full bg-[#252A41] rounded-tl-xl rounded-bl-xl text-xl text-[#DCDEF3] cursor-pointer z-50',
     lists: `pl-[28%] text-[#9EA2C6] flex-col gap-2 py-2 ${
       dropDown ? 'flex' : 'hidden'
-    } animate__animated animate__flipInX `
+    } animate__animated animate__flipInX `,
   };
 
   return (
--- client/src/Components/Navbar/Sidebar/Sidebar.jsx
+++ client/src/Components/Navbar/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import DownButtons from './DownButtons';
 function Sidebar({ menuPosition }) {
   const [sideBarData, setSideBarData] = useState(null);
   const styles = {
-    sideBar: `h-[85%] overflow-y-scroll  z-30 w-[223px] text-white bg-[#1E2235] flex flex-col items-start pl-[12px] py-[100px] font-semibold fixed ${menuPosition} transition-all duration-500 `
+    sideBar: `h-[85%] overflow-y-scroll  z-30 w-[223px] text-white bg-[#1E2235] flex flex-col items-start pl-[12px] py-[100px] font-semibold fixed ${menuPosition} transition-all duration-500 `,
   };
   useEffect(() => {
     fetch('https://c8-40-m-mern-kappa.vercel.app/api/sidebar')
